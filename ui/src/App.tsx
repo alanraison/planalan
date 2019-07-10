e0bffb69386a7b7db8e62cb0e56651f87fa19380
Alan Raison <alanraison@users.noreply.github.com>
2019-07-10 22:45:52 +0000

    Some changed

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import RedTheme from './RedTheme';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/planning" render={<div>Planning</div>}/>
+        <Route path="/resources" component={<div>Resources</div>}/>
+        <Route component={<Redirect path="/planning"/>}/>
+      </Switch>
+    </Router>
   );
 }
 
