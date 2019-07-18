802b8b38b80a08644351dcd3697318536aeba783
Alan Raison <alanraison@users.noreply.github.com>
2019-07-18 20:15:52 +0100

    Initial Project display, some visual enhancements
    Ability to update projects in detail view;
    A second example;
    sytle updates.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ProjectPlanning from './projectPlanning';
+import RedTheme from './RedTheme';
+import { MuiThemeProvider } from '@material-ui/core/styles'
 
 const Resources: React.FC = () => <div>Resources</div>;
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <CssBaseline/>
-      <Switch>
-        <Route path="/planning" component={ProjectPlanning}/>
-        <Route path="/resources" component={Resources}/>
-        <Route component={() => <Redirect to="/planning"/>}/>
-      </Switch>
-    </Router>
+    <MuiThemeProvider theme={RedTheme}>
+      <Router>
+        <CssBaseline/>
+        <Switch>
+          <Route path="/planning" component={ProjectPlanning}/>
+          <Route path="/resources" component={Resources}/>
+          <Route component={() => <Redirect to="/planning"/>}/>
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
--- ui/src/projectPlanning/Detail.tsx
+++ ui/src/projectPlanning/Detail.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import { PlannedProject } from '../api';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+  titleRow: {
+    display: 'flex',
+  },
+  titleField: {
+    paddingBottom: theme.spacing(4),
+    fontSize: 'large',
+    flex: 1,
+  },
+  formFields: {
+    margin: theme.spacing(1),
+  },
+}))
 
 const Detail: React.FC<{
-  project: PlannedProject
+  project: PlannedProject,
+  onChange: (project: PlannedProject) => void,
 }> = ({
   project,
-}) => (
-  <Paper>
-    {project.name}
-  </Paper>
-);
+  onChange,
+}) => {
+  const classes = useStyles();
+  // const [dirtyProject, setDirtyProject] = useState(project);
+
+  function handleChange(field: string, value: any) {
+    onChange({
+      ...project,
+      [field]: value,
+    })
+  }
+
+  return (
+    <Paper className={classes.root}>
+      <div className={classes.titleRow}>
+        <TextField
+          variant="filled"
+          className={clsx(classes.titleField, classes.formFields)}
+          label="Project name"
+          value={project.name}
+          onChange={(e) => handleChange('name', e.target.value)}
+        />
+        <TextField
+          variant="filled"
+          className={classes.formFields}
+          label="Project manager"
+          value={project.owner}
+          onChange={(e) => handleChange('owner', e.target.value)}
+        />
+      </div>
+    </Paper>
+  )
+};
 
 export default Detail;
