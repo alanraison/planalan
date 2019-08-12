e368bf23800be0cc75ba4b6c6c36fb68b7240e92
Alan Raison <alanraison@users.noreply.github.com>
2019-08-12 21:50:10 +0100

    Adding missing parameters

--- api/api.tf
+++ api/api.tf
@@ -8,8 +8,9 @@ resource "aws_api_gateway_rest_api" "planning_api" {
 }
 
 resource "aws_api_gateway_stage" "main" {
-  stage_name  = "v1"
-  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
+  stage_name    = "v1"
+  rest_api_id   = "${aws_api_gateway_rest_api.planning_api.id}"
+  deployment_id = "${aws_api_gateway_deployment.main.id}"
 }
 
 resource "aws_api_gateway_deployment" "main" {
@@ -26,6 +27,7 @@ data "aws_api_gateway_resource" "planned_projects" {
 resource "aws_api_gateway_integration" "mock" {
   rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
   resource_id = "${data.aws_api_gateway_resource.planned_projects.id}"
+  http_method = "GET"
   type        = "MOCK"
 }
 
