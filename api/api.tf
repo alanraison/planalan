b24ca0d3e113a6aa11934657148d39f997962d2f
Alan Raison <alanraison@users.noreply.github.com>
2019-08-13 21:28:20 +0100

    Mock response

--- api/api.tf
+++ api/api.tf
@@ -31,3 +31,20 @@ resource "aws_api_gateway_integration" "mock" {
   type        = "MOCK"
 }
 
+# resource "aws_api_gateway_method_response" "response_ok" {
+#   rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
+#   resource_id = "${data.aws_api_gateway_resource.planned_projects.id}"
+#   http_method = "GET"
+#   status_code = 200
+# }
+
+resource "aws_api_gateway_integration_response" "mock" {
+  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
+  resource_id = "${data.aws_api_gateway_resource.planned_projects.id}"
+  http_method = "GET"
+  status_code = 200
+
+  response_templates {
+    "application/json" = file("${path.module}/mockResponses/plannedProjects.json")
+  }
+}
