1e709da5142cdf48678a98686846121bece5ee5b
Alan Raison <alanraison@users.noreply.github.com>
2019-08-14 20:41:28 +0100

    Using amazon extensions for mock responses

--- api/api.tf
+++ api/api.tf
@@ -9,43 +9,12 @@ resource "aws_api_gateway_rest_api" "planning_api" {
 
 resource "aws_api_gateway_stage" "main" {
   stage_name    = "v1"
-  rest_api_id   = "${aws_api_gateway_rest_api.planning_api.id}"
-  deployment_id = "${aws_api_gateway_deployment.main.id}"
+  rest_api_id   = aws_api_gateway_rest_api.planning_api.id
+  deployment_id = aws_api_gateway_deployment.main.id
 }
 
 resource "aws_api_gateway_deployment" "main" {
   depends_on  = ["aws_api_gateway_integration.mock"]
-  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
+  rest_api_id = aws_api_gateway_rest_api.planning_api.id
   stage_name  = "v1"
-}
-
-data "aws_api_gateway_resource" "planned_projects" {
-  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
-  path        = "/plannedProjects"
-}
-
-resource "aws_api_gateway_integration" "mock" {
-  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
-  resource_id = "${data.aws_api_gateway_resource.planned_projects.id}"
-  http_method = "GET"
-  type        = "MOCK"
-}
-
-resource "aws_api_gateway_method_response" "response_ok" {
-  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
-  resource_id = "${data.aws_api_gateway_resource.planned_projects.id}"
-  http_method = "GET"
-  status_code = 200
-}
-
-resource "aws_api_gateway_integration_response" "mock" {
-  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
-  resource_id = "${data.aws_api_gateway_resource.planned_projects.id}"
-  http_method = "GET"
-  status_code = "${aws_api_gateway_method_response.response_ok.status_code}"
-  selection_pattern = "-"
-
-  response_templates = {
-    "application/json" = file("${path.module}/mockResponses/plannedProjects.json")
-  }
 }
