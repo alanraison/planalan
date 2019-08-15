07cfec872a8fcd00c865e4ff06f7243edac0935e
Alan Raison <alanraison@users.noreply.github.com>
2019-08-15 06:04:49 +0100

    removing invalid dependency

--- api/api.tf
+++ api/api.tf
@@ -9,12 +9,16 @@ resource "aws_api_gateway_rest_api" "planning_api" {
 
 resource "aws_api_gateway_stage" "main" {
   stage_name    = "v1"
-  rest_api_id   = aws_api_gateway_rest_api.planning_api.id
-  deployment_id = aws_api_gateway_deployment.main.id
+  rest_api_id   = "${aws_api_gateway_rest_api.planning_api.id}"
+  deployment_id = "${aws_api_gateway_deployment.main.id}"
 }
 
 resource "aws_api_gateway_deployment" "main" {
-  depends_on  = ["aws_api_gateway_integration.mock"]
-  rest_api_id = aws_api_gateway_rest_api.planning_api.id
+  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
   stage_name  = "v1"
+}
+
+data "aws_api_gateway_resource" "planned_projects" {
+  rest_api_id = "${aws_api_gateway_rest_api.planning_api.id}"
+  path        = "/plannedProjects"
 }
